1f11f20c524c5b16af378ba61ea72317f8c9ca2a
TANIGUCHI Masaya <mew@cat.ovh>
2020-06-03 21:54:12 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,104 @@
+# Rehype Template
+
+Rehype plugin to wrap contents with [htm](https://github.com/developit/htm) template
+
+## Installation
+
+    npm install remark-frontmatter remark-extract-frontmatter rehype-template
+
+## Usage
+
+Say `example.md` looks as follows;
+
+```markdown
+---
+title: Example
+---
+# Example
+This is example
+```
+
+...and `example.js` like this:
+
+```javascript
+const vfile = require("to-vfile")
+const report = require("vfile-report")
+const unified = require("unified")
+const remark = require("remark-parse")
+const remark2rehype = require("remark-rehype")
+const frontmatter = require("remark-frontmatter")
+const extract = require("remark-extract-frontmatter")
+const stringify = require("rehype-stringify")
+const { parse } = require("yaml")
+const { html, template } = require("rehype-template")
+
+const t = (node, frontmatter) => html`
+    <html>
+        <head>
+            <title>${frontmatter.title}</title>
+        </head>
+        <body>${node}</body>
+    </html>
+`
+
+unified()
+    .use(remark)
+    .use(frontmatter)
+    .use(extract, {yaml: parse})
+    .use(remark2rehype)
+    .use(template, {template: t})
+    .use(stringify)
+    .process(vfile.readSync("example.md"), (err, file) => {
+        console.error(report(err || file))
+        console.log(String(file))
+    })
+```
+
+Now, running `node example` yields:
+
+```html
+<html><head><title>Example</title></head><body><h1>Example</h1>
+<p>This is example</p></body></html>
+```
+
+## API
+
+### `rehype().use(template[, options])`
+
+Wrap the content with `options.template`, which uses *template literal*.
+
+- `options.template(node, frontmatter)`
+    - `node` Syntax tree
+    - `frontmatter` Object as frontmatter parsed by `remark-frontmatter` and `remark-extract-frontmatter`.
+    - This function returns new syntax tree.
+
+### `html`
+
+Template literal function defined as `htm.bind(h)` where `h` is [hastscript](https://github.com/syntax-tree/hastscript).
+
+```javascript
+> const name = "world"
+> html`<p>hello, ${name}</p>`
+```
+
+The above code yields:
+
+```javascript
+{
+  type: 'element',
+  tagName: 'p',
+  properties: {},
+  children: [
+    { type: 'text', value: 'hello, ' },
+    { type: 'text', value: 'world' }
+  ]
+}
+```
+
+## Security
+
+Use of `rehype-template` should be safe to use as `htm` should be safe to use. When in doubt, use `rehype-sanitize`.
+
+## License
+
+MIT &copy; TANIGUCHI Masaya
---
+++ index.test.js
@@ -0,0 +1,77 @@
+/**
+ * @license
+ * Copyright (c) 2020 TANIGUCHI Masaya.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+const unified = require("unified")
+const remark = require("remark-parse")
+const remark2rehype = require("remark-rehype")
+const frontmatter = require("remark-frontmatter")
+const extract = require("remark-extract-frontmatter")
+const stringify = require("rehype-stringify")
+const { parse } = require("yaml")
+const { html, template } = require(".")
+
+test("Template with options", () => {
+    const i = `
+---
+title: test
+---
+# test
+test`;
+
+    const t = (node, frontmatter) => html`
+        <html>
+            <head>
+                <title>${frontmatter.title}</title>
+            </head>
+            <body>${node}</body>
+        </html>
+    `;
+
+    const o = unified()
+        .use(remark)
+        .use(frontmatter)
+        .use(extract, {yaml: parse})
+        .use(remark2rehype)
+        .use(template, {template: t})
+        .use(stringify)
+        .processSync(i)
+    expect(o.toString()).toBe("<html><head><title>test</title></head><body><h1>test</h1>\n<p>test</p></body></html>")
+})
+
+test("Template without options", () => {
+    const i = `
+---
+title: test
+---
+# test
+test`;
+
+    const o = unified()
+        .use(remark)
+        .use(frontmatter)
+        .use(extract, {yaml: parse})
+        .use(remark2rehype)
+        .use(template)
+        .use(stringify)
+        .processSync(i)
+    expect(o.toString()).toBe("<h1>test</h1>\n<p>test</p>")
+})
